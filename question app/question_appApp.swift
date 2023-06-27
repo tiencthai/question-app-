a0cf256710fe915657126db2a3e9c162179d1379
Tien <tiencthai2006@gmaik.com>
2023-06-27 11:19:26 -0400

    Initial Commit

---
+++ question app/question_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  question_appApp.swift
+//  question app
+//
+//  Created by Tien Thai on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct question_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
